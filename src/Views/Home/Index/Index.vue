20e284ab7bf81323cff92dd22b7a4bcd4c4c66fd
Matthew Bitter <contact@matthewbitter.com>
2019-02-24 15:51:31 -0500

    Add GitHub Link

--- src/Views/Home/Index/Index.vue
+++ src/Views/Home/Index/Index.vue
@@ -3,6 +3,9 @@
         <div>
             <h1>MATTHEW BITTER</h1>
             <h2>WEB & GAME DEVELOPER</h2>
+            <a href="//github.com/matthewbitter" title="GitHub">
+                <img src="@/Assets/GitHub.png" />
+            </a>
             <a class="button" href="MatthewBitterResume.docx" title="Download Resume">Resume</a>
         </div>
     </div>
