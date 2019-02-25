9435de4dbf5143e3f05f1295e3f5b21b1f78524e
Matthew Bitter <contact@matthewbitter.com>
2019-02-24 20:24:56 -0500

    LinkedIn & Resume Icons

--- src/Views/Home/Index/Index.vue
+++ src/Views/Home/Index/Index.vue
@@ -3,10 +3,16 @@
         <div>
             <h1>MATTHEW BITTER</h1>
             <h2>WEB & GAME DEVELOPER</h2>
+            <hr />
             <a href="//github.com/matthewbitter" title="GitHub">
                 <img src="@/Assets/GitHub.png" />
             </a>
-            <a class="button" href="MatthewBitterResume.docx" title="Download Résumé">Résumé</a>
+            <a href="//www.linkedin.com/in/matthewbitterdeveloper/" title="LinkedIn ">
+                <img src="@/Assets/LinkedIn.png" />
+            </a>
+            <a href="MatthewBitterResume.docx" title="Resume">
+                <img src="@/Assets/Resume.png" />
+            </a>
         </div>
     </div>
 </template>
