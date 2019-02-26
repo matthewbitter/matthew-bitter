a94c09e2500257788f8a2fedf5f9bad5b3f5202b
Matthew Bitter <contact@matthewbitter.com>
2019-02-26 09:37:40 -0500

    img alt attributes

--- src/Views/Home/Index/Index.vue
+++ src/Views/Home/Index/Index.vue
@@ -5,13 +5,13 @@
             <h2>WEB & GAME DEVELOPER</h2>
             <hr />
             <a href="//github.com/matthewbitter" title="GitHub">
-                <img src="@/Assets/GitHub.png" />
+                <img src="@/Assets/GitHub.png" alt="GitHub" />
             </a>
             <a href="//www.linkedin.com/in/matthewbitterdeveloper/" title="LinkedIn ">
-                <img src="@/Assets/LinkedIn.png" />
+                <img src="@/Assets/LinkedIn.png" alt="LinkedIn" />
             </a>
             <a href="MatthewBitterResume.docx" title="Resume">
-                <img src="@/Assets/Resume.png" />
+                <img src="@/Assets/Resume.png" alt="Resume" />
             </a>
         </div>
     </div>
