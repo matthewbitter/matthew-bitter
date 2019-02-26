fb570b11ec52f225ef8b3a48b65a654d76aac21e
Matthew Bitter <contact@matthewbitter.com>
2019-02-26 12:29:48 -0500

    vuetify clean up

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import store from "@/Modules/Vuex/Vuex";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./Modules/Router";
-import "./Plugins/vuetify";
+//import "./Plugins/vuetify";
 
 Vue.config.productionTip = false;
 
